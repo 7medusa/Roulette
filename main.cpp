487d515b19a53197e3ec4c8df7f4a32ed0986811
7medusa <wallakevin2005@gmail.com>
2024-02-08 21:22:16 +0100

    update

--- main.cpp
+++ main.cpp
@@ -8,16 +8,22 @@
 
 //arrays for the different winner types
 int total[37];
+int even[30];
+int odd[30];
 int red[5] = {1, 3, 5, 7, 9};
 int black[5] = {2, 4, 6, 8};
+//1to18
+//19to36
 
 int score = 10;
 int max_length_total = sizeof(total) / sizeof(total[0]) - 1;
 
-void array_f(int array[], int min, int max) {//function to get information into arrays
-    int i;
-    for(i = min; i <= max; i++) {
-        array[i] = i;
+void array_f(int array[], int min, int max, int step = 1) {//function to get information into arrays
+    int i = 0;
+    int n;
+    for(n = min; n <= max; n += step) {
+        array[i] = n;
+        i++;
     }
 }
 
@@ -30,13 +36,16 @@ int random_f() {
 
 int input_f() {
     int input;
-    printf("eine zahl zwischen 0 und 9, 10 fuer rot und 11 fuer schwarz, 12 fuer verlassen\n");
+    printf("eine zahl zwischen 0 und 9, 37 fuer rot und 38 fuer schwarz, 39 für odd, 40 für even, 42 fuer verlassen\n");
     printf("ihre eingabe:\n>");
     scanf("%d", &input);//input for the number to bet
     getchar();
 
     if(input < 0 || input > 11) {
         return -1;//false input
+    }
+    else if(input == 42) {
+        return -1;
     }
     else {
         return input;
@@ -71,14 +80,24 @@ void run() {//main function
             printf("good bye");
             return;
         }
-        else if (input == 10) {
+        else if (input == 37) {
             score_input = scoreabzug_f();
             printf("rot");//debugging mit for schleife jeden teil des array überprüfen
             return;//debugging
         }
-        else if (input == 11) {
+        else if (input == 38) {
             score_input = scoreabzug_f();
             printf("schwarz");//debugging
+            return;//debugging
+        }
+        else if (input == 39) {
+            score_input = scoreabzug_f();
+            printf("odd");//debugging
+            return;//debugging
+        }
+        else if (input == 40) {
+            score_input = scoreabzug_f();
+            printf("even");//debugging
             return;//debugging
         }
         else {
@@ -97,8 +116,11 @@ void run() {//main function
 }
 
 int main() {
-    array_f(total, 0, 36);//array gesamt
-    //weitere arrays definieren
+    //define arrays
+    array_f(total, 0, 36);
+    array_f(even, 2, 36, 2);
+    array_f(odd, 1, 36, 2);
+
     run();
     return 0;
 }
